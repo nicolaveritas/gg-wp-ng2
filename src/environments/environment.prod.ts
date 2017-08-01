182e67321adf69ea66063d639cde14ced35b9d39
6i4NNi <6i4NNi>
2017-08-01 19:44:24 +0200

    updated env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_path_dev: "http://g53eu38l5cf.x10host.com/wp-json/wp/v2",
-  base_path_prod: "http://giuseppinagiordano.com/wp-json/wp/v2"
+  base_path_prod: "http://www.giuseppinagiordano.com/wp-json/wp/v2"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   base_path_dev: "http://g53eu38l5cf.x10host.com/wp-json/wp/v2",
-  base_path_prod: "http://giuseppinagiordano.com/wp-json/wp/v2"
+  base_path_prod: "http://www.giuseppinagiordano.com/wp-json/wp/v2"
 };
